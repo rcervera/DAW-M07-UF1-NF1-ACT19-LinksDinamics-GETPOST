a0070375ad13fcf97b32fbd92031253acf0bede9
Ramon Cervera <rcerver4@xtec.cat>
2020-10-05 17:33:56 +0200

    establerts límnits en número links

--- linksdinamics.php
+++ linksdinamics.php
@@ -17,6 +17,17 @@ Entra el número de links que vols crear:
 if(isset($_REQUEST['numero']))
 {
     $numero = $_REQUEST['numero'];
+
+    if(is_numeric($numero)===FALSE) {
+        echo "El valor no és numèric!";
+        exit;
+    }
+
+    if($numero<1 || $numero>100) {
+        echo "El valor de N ha der ser un número entre 1 i 100";
+        exit;
+    }
+
     for($i=1; $i <= $numero; $i++) {
         echo "<a href='".$_SERVER['PHP_SELF']."?numero=".
             $numero."&n=".$i."'>pagina ".$i."</a><br>";
