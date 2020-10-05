770b8322caf80d062a18dc76c067ef18d163cea8
Ramon Cervera <rcerver4@xtec.cat>
2020-10-05 18:39:03 +0200

    establerts límnits en número links

--- linksdinamics.php
+++ linksdinamics.php
@@ -3,9 +3,9 @@
 </head>
 <body>
 Entra el número de links que vols crear:
-<form action='<?php echo $_SERVER['PHP_SELF']; ?>' method='POST'>
+<form action='<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>' method='POST'>
     <input type='text' name='numero'
-    <?php if(isset($_REQUEST['numero'])) echo " value= ".$_REQUEST['numero']." ";
+    <?php if(isset($_REQUEST['numero'])) echo " value= ".htmlspecialchars($_REQUEST['numero'])." ";
     ?>
 
     >
@@ -16,7 +16,7 @@ Entra el número de links que vols crear:
 
 if(isset($_REQUEST['numero']))
 {
-    $numero = $_REQUEST['numero'];
+    $numero = htmlspecialchars($_REQUEST['numero']);
 
     if(is_numeric($numero)===FALSE) {
         echo "El valor no és numèric!";
@@ -29,14 +29,15 @@ if(isset($_REQUEST['numero']))
     }
 
     for($i=1; $i <= $numero; $i++) {
-        echo "<a href='".$_SERVER['PHP_SELF']."?numero=".
+        echo "<a href='".htmlspecialchars($_SERVER['PHP_SELF'])."?numero=".
             $numero."&n=".$i."'>pagina ".$i."</a><br>";
     }
 }
 
 if(isset($_GET['n']))
 {
-    echo "<br>Has fet click sobre enllaç ". $_GET['n'];
+    $n = htmlspecialchars($_GET['n']);
+    echo "<br>Has fet click sobre enllaç ". $n;
 }
 
 ?>
